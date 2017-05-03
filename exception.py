26d5dc4203ae4cd457288242ea7055981850b5d7
Harsh Tomar <harshtomar6@gmail.com>
2017-05-03 11:33:36 +0530

    Second

---
+++ exception.py
@@ -0,0 +1,8 @@
+
+class exceptionManager:
+    def holidayException(self):
+        print("Got it! Today is holiday")
+    def classNotHappenedException(self):
+        print("Gotch yaa")
+    def anotherClassHappenedException(self, currentClass, anotherClass):
+        pass
